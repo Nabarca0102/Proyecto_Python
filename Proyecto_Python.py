52e271d5215ba9cdb176d7e9d79046f7b65d7769
Nabarca0102 <82837937+Nabarca0102@users.noreply.github.com>
2021-12-01 14:33:30 -0600

    Add files via upload
    guia estilos

--- Proyecto_Python.py
+++ Proyecto_Python.py
@@ -3,11 +3,18 @@
 # Curso: Programación bajo plataformas abiertas
 # Estudinates:
 # Juarez Moraga Adonay B74047
-# Abarca Obregón Nelber
-# Prado Francklin
+# Abarca Obregón Nelber B60022
+# Calderon Prado Franklin B81405
 
 '''
-Este programa es la elaboración del juego Snake
+   Este programa es la elaboración del juego Snake
+   elaborado con la biblioteca turtle. Describiendo
+   de forma brete el codigo, se crean una serie de 
+   métodos y funciones para crear la ventana del 
+   juego, la apariencia de la interfaz, la comida 
+   y en general todas las funcionalidades del juego.
+   Todo esto posteriormente se introduce en un bucle
+   para que el juego corra hasta que el usuario pierda  
 '''
 
 
@@ -17,16 +24,22 @@ import time
 import random
 
 atrasar = 0.1
+lista = []
+obstaculos=[]
 
+# Variables de conteo puntos
+score = 0
+high_score = 0
+serpiente_total = len(lista)
+        
 # Se configura la ventana
 # Se declara variable ventana.
 # Se configura para que muestre color, titulo y demas. 
 ventana = turtle.Screen()
-ventana.title('Juego de snake')
+ventana.title('Juego Snake')
 ventana.bgcolor('green')
 ventana.setup(width= 600, height=600)
 ventana.tracer(0)
-
 
 # Se configura la cabeza de la serpiente.
 # Se añade color, direcion y demas. 
@@ -38,15 +51,13 @@ cabeza.penup()
 cabeza.goto(0,0)
 cabeza.direction = 'stop'
 
-
 # Definir los metodos de comida
 comida = turtle.Turtle()
 comida.speed(0)
 comida.shape('circle')
 comida.color('red')
 comida.penup()
-comida.goto(0,0)
-comida.direction = 'stop'
+comida.goto(random.randint(-280,280),random.randint(-280,260))
 
 
 # Funcion para las teclas.
@@ -62,6 +73,7 @@ def abajo():
 
 # Se define funciones de movimiento
 def movimiento():
+
     if cabeza.direction == "up":
         # Cordenada y
         y = cabeza.ycor()
@@ -86,31 +98,76 @@ ventana.onkeypress(abajo, "Down")
 ventana.onkeypress(izquierda, "Left")
 ventana.onkeypress(derecha, "Right")
 
-# Se crea una lista nueva
-# Se agrega segmentación a la serpiente.
-lista = []
+
+# Metodos imprimir texto en pantalla
+texto = turtle.Turtle()
+texto.speed(0)
+texto.color("black")
+texto.penup()
+texto.hideturtle()
+texto.goto(-280, 270)
+
+# Se define el texto de fin de juego
+GameOver= turtle.Turtle()
+GameOver.speed(0)
+GameOver.color("black")
+GameOver.penup()
+GameOver.hideturtle()
+GameOver.goto(-146,0)
+
 
 while True:
     ventana.update()
-    # Coliciones con la pared
-    if cabeza.xcor() > 280:
-        cabeza.goto(-300,0)
 
-
+    # Coliciones con las paredes
+    if cabeza.xcor() > 300:
+        cabeza.goto(-300, y)
+    
+    elif cabeza.xcor() < -300:
+        cabeza.goto(300, y)
+        
+    elif cabeza.ycor() > 300:
+        cabeza.goto(x, -300)
+    
+    elif cabeza.ycor() < -300:
+        cabeza.goto(x, 300)
+    
 
     # Coliciones con la comida 
     if cabeza.distance(comida) < 20:
         cor_x = random.randint(-280,280)
-        cor_y = random.randint(-280,280)
+        cor_y = random.randint(-280,260)
         comida.goto(cor_x, cor_y)
 
+        # Nuevo segmento tras comer
         siguinte_cabe = turtle.Turtle()
         siguinte_cabe.speed(0)
         siguinte_cabe.shape('square')
         siguinte_cabe.color('grey')
         siguinte_cabe.penup()
         lista.append(siguinte_cabe)
-    
+
+        # Se agrega un obstáculo cada 4 elementos 
+        c = len(lista)
+        if c%4 == 0:
+            obstaculo = turtle.Turtle()
+            obstaculo.speed(0)
+            obstaculo.shape('square')
+            obstaculo.color('blue')
+            obstaculo.penup()
+            obstaculo.goto(random.randint(-280,280),random.randint(-280,260))
+            obstaculos.append(obstaculo)
+                
+        
+        # Contador de puntos
+        score += 10
+        if score > high_score:
+            high_score = score
+            
+        texto.clear()
+        texto.write("Puntos: {}  Record: {}".format(score, high_score), 
+                    font=("Courier", 16))
+        
     # Se asigana siguiente_cabe para extender cabeza
     serpiente_total = len(lista)
 
@@ -120,17 +177,64 @@ while True:
         cor_y = lista[i - 1].ycor()
         lista[i].goto(cor_x, cor_y)
 
+
     # Se implememta condicional para añadir segmentos
     if serpiente_total > 0:
         x = cabeza.xcor()
         y = cabeza.ycor()
         lista[0].goto(x, y)
 
- 
-
-
         
-
-
     movimiento()
+    
+    # Colisiones con el cuerpo
+    for segmento in lista:
+        if segmento.distance(cabeza) < 20:
+            GameOver.write("Game Over", font =("Courier",40, "normal"))
+            time.sleep(1)
+            cabeza.goto(0,0)
+            cabeza.direction = "stop"
+            GameOver.clear()
+            
+            # Escondemos segmentos
+            for segmento in lista:
+                segmento.goto(1000, 1000)
+            lista.clear()
+
+            # Escondemos obstaculos
+            for obstaculo in obstaculos:
+                obstaculo.goto(1000, 1000)
+            obstaculos.clear()
+               
+            # Reinicio del marcador
+            score = 0
+            texto.clear()
+            texto.write("Puntos: {}  Record: {}".format(score, high_score), 
+                        font=("Courier", 16))
+            
+ # Colisiones con los obstáculos
+    for obstaculo in obstaculos:
+        if obstaculo.distance(cabeza) < 20:
+            GameOver.write("Game Over", font =("Courier",40, "normal"))
+            time.sleep(1)
+            cabeza.goto(0,0)
+            cabeza.direction = "stop"
+            GameOver.clear()
+
+            # Escondemos segmentos
+            for segmento in lista:
+                segmento.goto(1000, 1000)
+            lista.clear()
+            
+            # Escondemos obstaculos
+            for obstaculo in obstaculos:
+                obstaculo.goto(1000, 1000)
+            obstaculos.clear()
+               
+            # Reinicio del marcador
+            score = 0
+            texto.clear()
+            texto.write("Puntos: {}  Record: {}".format(score, high_score), 
+                        font=("Courier", 16))
+    
     time.sleep(atrasar)
